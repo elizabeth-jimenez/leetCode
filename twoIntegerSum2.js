146d99786900fd7a434acf20b4279fd06ae793f0
Elizabeth Jimenez <elizabeth.jimenez.cs@gmail.com>
2024-09-14 13:43:40 -0400

    Optimization for Two Integer Sum 2

--- twoIntegerSum2.js
+++ twoIntegerSum2.js
@@ -1,11 +1,16 @@
 function twoSum(numbers, target) {
     for(let i = 0; i < numbers.length - 1; i++) {
         for(let j = i + 1; j < numbers.length; j++) {
-            if(numbers[i] + numbers[j] === target) {
+            const sum = numbers[i] + numbers[j];
+            if(sum > target) {
+                j = numbers.length;
+            }
+            if(sum === target) {
                 return [i + 1, j + 1];
             }
         }
     }
 }
 
+console.log(twoSum([1,2,3,4,8,9,10,13], 7)); // [3,4]
 console.log(twoSum([1,2,3,4], 3)); // [1,2]
